bcd10fb9454d5e1cdbef68650890c8a6cf171bd8
SerhiiBruj <sergijk174@gmail.com>
2024-08-18 18:55:22 +0300

    merging

--- BrowserApp/src/mainPageModules/leftSidePageNavbarComponents/profile.jsx
+++ BrowserApp/src/mainPageModules/leftSidePageNavbarComponents/profile.jsx
@@ -1,22 +1,26 @@
-import UnknownUserIcon from '../../assetModules/svgs/unknownUser'
-import PenIcon from '../../assetModules/svgs/pen'
+import UnknownUserIcon from '../../assetModules/svgs/unknownUser';
+import PenIcon from '../../assetModules/svgs/pen';
+import bg from '../../assetModules/noSvg/bg.jpg'; // Переконайтесь, що шлях до зображення правильний
 
 const Profile = () => {
-
-
-
-  
   return (
-      <div className="profile">
-        <div className="profileImgConteiner">
-        <div className="profileImg">
-          <UnknownUserIcon />
+    <div className="profile">
+      <div className="profileImgConteiner">
+        <div
+          className="profileImg"
+          style={{
+            backgroundImage: `url(${bg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          {!bg&&<UnknownUserIcon />}
         </div>
-        <PenIcon color={"#2e2e2e"} allow={true}  size={0.7}/>
-        </div>
-        <p className="profileEmail">unknown@zhmil.com</p>
+        <PenIcon color="#2e2e2e" allow={true} size={0.7} />
       </div>
-  )
-}
+      <p className="profileEmail">unknown@zhmil.com</p>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
